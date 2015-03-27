af2ddd3f188bef4a2bc0699af33f5ebc669246c5
Tomasz Lichon <lichon@student.agh.edu.pl>
2015-03-27 14:04:54 +0200

    README + some vars extracted

--- bamboos/docker/demo_up.py
+++ bamboos/docker/demo_up.py
@@ -3,37 +3,47 @@
 import os
 from environment import common, docker
 
-uid = common.generate_uid()
-dns, dns_output = common.set_up_dns('auto', 'onedata')
+#===== input =======
+sl_builder_image = 'onedata/sl_builder:v2'
+builder_image = 'onedata/worker'
 
+config_dir = '/home/lichon/IdeaProjects/oneprovider/dev_scripts/cfg'
+
+globalregistry_pkg_dir = '/home/lichon/IdeaProjects/globalregistry/rel'
+globalregistry_pkg_name = 'globalregistry-Linux.x86_64.rpm'
+
+provider_pkg_dir = '/home/lichon/IdeaProjects/oneprovider/releases'
+provider_pkg_name = 'oneprovider_2.5.0.7.deb'
+#===================
+
+dns, dns_output = common.set_up_dns('auto', 'onedata')
 gr_name = 'gr_onedata'
 gr = docker.run(
-    image='onedata/sl_builder:v2',
+    image=sl_builder_image,
     hostname='gr.onedata.dev.docker',
     detach=True,
     interactive=True,
     tty=True,
     workdir='/root',
     name=gr_name,
-    volumes=[('/home/lichon/IdeaProjects/globalregistry/rel', '/root/pkg', 'ro'),
-             ('/home/lichon/IdeaProjects/oneprovider/dev_scripts/cfg', '/root/cfg', 'ro')],
+    volumes=[(globalregistry_pkg_dir, '/root/pkg', 'ro'),
+             (config_dir, '/root/cfg', 'ro')],
     dns_list=dns,
-    # link={db_dockername: db_hostname},
-    command='yum install -y pkg/globalregistry-Linux.x86_64.rpm && sleep 5 && onepanel_admin --install /root/cfg/gr.cfg ; bash')
+    command='yum install -y pkg/' + globalregistry_pkg_name + ' && sleep 5 && onepanel_admin --install /root/cfg/gr.cfg ; bash')
 
 provider1 = docker.run(
-    image='onedata/worker',
+    image=builder_image,
     hostname='provider1.onedata.dev.docker',
     detach=True,
     interactive=True,
     tty=True,
     workdir='/root',
     name='provider1_onedata',
-    volumes=[('/home/lichon/IdeaProjects/oneprovider/releases', '/root/pkg', 'ro'),
-             ('/home/lichon/IdeaProjects/oneprovider/dev_scripts/cfg', '/root/cfg', 'ro')],
+    volumes=[(provider_pkg_dir, '/root/pkg', 'ro'),
+             (config_dir, '/root/cfg', 'ro')],
     dns_list=dns,
     link={gr_name: 'onedata.org'},
-    command='''dpkg -i pkg/oneprovider_2.5.0.7.deb || apt-get -y install -f
+    command='dpkg -i pkg/' + provider_pkg_name + ''' || apt-get -y install -f
 sed -i \"s/{239, 255, 0, 1}/{238, 255, 0, 1}/g\" /opt/oneprovider/nodes/onepanel/etc/app.config
 apt-get -y install libnspr4-dev
 sleep 5
@@ -41,18 +51,18 @@ onepanel_admin --install /root/cfg/prov1.cfg
 bash''')
 
 provider2 = docker.run(
-    image='onedata/worker',
+    image=builder_image,
     hostname='provider2.onedata.dev.docker',
     detach=True,
     interactive=True,
     tty=True,
     workdir='/root',
     name='provider2_onedata',
-    volumes=[('/home/lichon/IdeaProjects/oneprovider/releases', '/root/pkg', 'ro'),
-             ('/home/lichon/IdeaProjects/oneprovider/dev_scripts/cfg', '/root/cfg', 'ro')],
+    volumes=[(provider_pkg_dir, '/root/pkg', 'ro'),
+             (config_dir, '/root/cfg', 'ro')],
     dns_list=dns,
     link={gr_name: 'onedata.org'},
-    command='''dpkg -i pkg/oneprovider_2.5.0.7.deb || apt-get -y install -f
+    command='dpkg -i ' + provider_pkg_name + ''' || apt-get -y install -f
 apt-get -y install libnspr4-dev
 sleep 5
 onepanel_admin --install /root/cfg/prov2.cfg
